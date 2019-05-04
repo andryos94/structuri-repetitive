3aa64b33b23ce37e24d9945685d9c82d65593529
andryos94 <andreistefan9422@gmail.com>
2019-05-04 23:11:22 +0300

    Ex1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
-        for(int i=1; i<11; i++){
-            System.out.println("Count is: " + i);
+        for (int i = 1; i < 8; i++) {
+            if ( (i == 1) || (i == 7) ) {
+                System.out.print("# ");
+            } else if ((i == 2) || (i == 6)) {
+                System.out.print("## ");
+            } else if ((i == 3) || (i == 5)) {
+                System.out.print("### ");
+            } else {
+                System.out.print("#### ");
+            }
         }
     }
 }
