369f3f9ccb192c4378c39eb399eea52aa061f077
andryos94 <andreistefan9422@gmail.com>
2019-05-05 10:08:14 +0300

    Ex6 - primele N numere naturale impare

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
-        int n = 10;
-        while (n != 0) {
-            System.out.print(n + " ");
-            n--;
-        }
+        int n = 15;
+        int count = 0;
+        int i = 0;
+            do {
+                if(i%2 != 0) {
+                    count++;
+                    System.out.print(i + " ");
+                }
+                i++;
+            } while (count < n);
     }
 }
