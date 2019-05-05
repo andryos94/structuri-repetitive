2aea5335f91edfad72f8986c8ab489fccebf485a
andryos94 <andreistefan9422@gmail.com>
2019-05-05 10:51:01 +0300

    Ex8 - a la puterea b

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,13 @@
 public class Main {
-    static int power(int x, int y) {
-        int prod = 1;
-        while (y != 0) {
-            prod = x * prod;
-            y--;
-        }
-        return prod;
-    }
-    ////metoda, ridicarea la putere, x^y
 
     public static void main(String[] args) {
-        int n = 6;
-        long sum = 0;
-        long newNo;
-        newNo = (long) (power((2*3),n));
-        System.out.println(newNo);
-        long r; //restul impartirii la 10
-        long c = newNo; //catul impartirii la 10
-        while (c != 0) {
-            r = newNo%10;
-            c = newNo/10;
-            newNo = c;
-            sum = sum + r;
+        int a = 5;
+        int b = 2;
+        int prod = 1;
+        while (b != 0) {
+            prod = a * prod;
+            b--;
         }
-        System.out.println(sum);
+        System.out.println(prod);
     }
 }
