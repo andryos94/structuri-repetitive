b1b09ce13d4bb43702d2abca106e335e757285b8
andryos94 <andreistefan9422@gmail.com>
2019-05-05 10:44:11 +0300

    Ex7 - suma cifrelor unui numar/power method

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
 public class Main {
+    static int power(int x, int y) {
+        int prod = 1;
+        while (y != 0) {
+            prod = x * prod;
+            y--;
+        }
+        return prod;
+    }
+    ////metoda, ridicarea la putere, x^y
 
     public static void main(String[] args) {
-        int n = 15;
-        int count = 0;
-        int i = 0;
-            do {
-                if(i%2 != 0) {
-                    count++;
-                    System.out.print(i + " ");
-                }
-                i++;
-            } while (count < n);
+        int n = 6;
+        long sum = 0;
+        long newNo;
+        newNo = (long) (power((2*3),n));
+        System.out.println(newNo);
+        long r; //restul impartirii la 10
+        long c = newNo; //catul impartirii la 10
+        while (c != 0) {
+            r = newNo%10;
+            c = newNo/10;
+            newNo = c;
+            sum = sum + r;
+        }
+        System.out.println(sum);
     }
 }
