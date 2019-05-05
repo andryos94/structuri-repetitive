2ee4e8022d51d31d9fbdf69fc4a3589bd5b04238
andryos94 <andreistefan9422@gmail.com>
2019-05-05 11:40:09 +0300

    Ex10 - suma primelor 5 elemente pare

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,20 @@
 public class Main {
 
     public static void main(String[] args) {
-        int n = 7;
-        for (int i=1;i<=n;i++) {
-            for (int k=1;k<=i;k++) {
-                System.out.print(k + " ");
-                if (k == i){
-                    System.out.print("\n");
+        int[] numberArray = new int[]{2,1,10,6,3,9,56,33,25,2,56,3,1,4};
+        int sum = 0;
+        int k = 0;
+        for (int i=0;i<numberArray.length;i++) {
+            while (k<5) {
+                if (numberArray[i]%2 == 0) {
+                    sum = sum + numberArray[i];
+                    k++;
+                    break;
+                } else {
+                    break;
                 }
             }
         }
+        System.out.println(sum);
     }
 }
