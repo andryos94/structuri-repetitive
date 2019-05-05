275d5fa837fd704991e291bae6474bf324fa5518
andryos94 <andreistefan9422@gmail.com>
2019-05-05 09:44:52 +0300

    Ex5 - ordine desc nr nat nenule

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-        int[] firstArray = new int[]{6,10,3,7,1,0,20,19,32};
-        int l = firstArray.length;
-        int count_pare = 0;
-        int count_impare = 0;
-        for (int i=0;i<l;i++) {
-            if(firstArray[i]%2 != 0) {
-                count_impare++;
-            } else {
-                count_pare++;
-            }
+        int n = 10;
+        while (n != 0) {
+            System.out.print(n + " ");
+            n--;
         }
-        System.out.println("Exista " + count_impare + " numere impare");
-        System.out.println("Exista " + count_pare + " numere pare");
     }
 }
