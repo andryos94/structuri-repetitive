70f4cabc801055123708cdfd51fca55e3cc5f354
andryos94 <andreistefan9422@gmail.com>
2019-05-05 16:23:25 +0300

    Ex13 - afisare go de n ori

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
-        //varianta cu switch
-        int month = 12;
-        String monthString = "";
-        switch (month) {
-            case 1: monthString = "Ianuarie";
-                    break;
-            case 2: monthString = "Februarie";
-                    break;
-            case 3: monthString = "Martie";
-                    break;
-            case 4: monthString = "Aprilie";
-                    break;
-            case 5: monthString = "Mai";
-                    break;
-            case 6: monthString = "Iunie";
-                    break;
-            case 7: monthString = "Iulie";
-                    break;
-            case 8: monthString = "August";
-                    break;
-            case 9: monthString = "Septembrie";
-                    break;
-            case 10: monthString = "Octombrie";
-                    break;
-            case 11: monthString = "Noiembrie";
+        int n = 10;
+        for (int i=0;i<=n;i++) {
+            if (i==n) {
+                System.out.println("done");
                 break;
-            case 12: monthString = "Decembrie";
-                break;
-            default: System.out.println("luna inexistenta");
-                    break;
+            }
+            System.out.println("go");
         }
-        System.out.println(month + ": " + monthString);
     }
 }
