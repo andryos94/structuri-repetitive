cdfea3b6b8622c061f1da3e405b518504a370ccb
andryos94 <andreistefan9422@gmail.com>
2019-05-05 23:44:03 +0300

    Ex18 - char

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
-        int n = 1234567;
-        int i = 0;
-        while (n != 0) {
-            n = n/10;
-            i++;
+        char n = 'S';
+        if (n == 'N' || n == 'S' || n == 'E' || n == 'V') {
+            System.out.println(n + " indica un punct cardinal");
+        } else {
+            System.out.println(n + " indica un punct cardinal");
         }
-        System.out.println(i);//digits number
 
-        int[] digitsArray = new int[i];
-        int c = 1234567;
-        int sum = 0;
-        while (c != 0) {
-            digitsArray[i-1] = c%10;
-            if ((i-1)%2 == 0) {
-                sum = sum + digitsArray[i-1];
-            }
-            c = c/10;
-            i--;
-        }
-        System.out.println(sum);//suma cifrelor numarului n de pe pozitii pare
     }
 }
