39f4228f8b005dcfdcc659f067941a35011cc2f2
andryos94 <andreistefan9422@gmail.com>
2019-05-05 23:00:30 +0300

    Ex15 - m este inversul lui n

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,21 @@
 public class Main {
 
     public static void main(String[] args) {
-        int n = 6;
-        while (n>5) {
-            for (int i=0;i<n;i++) {
-                System.out.println("going");
-            }
-            System.out.println();
+        int n = 134;
+        int m = 431;
+        int invers = 0;
+        int r;
+        int c = n;
+        while (c != 0) {
+            r = c%10;
+            c = c/10;
+            invers = invers*10 + r;
+        }
+        System.out.println(invers);
+        if (invers == m) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
         }
     }
 }
